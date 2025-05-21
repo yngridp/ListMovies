2aa41bfae009296a16bba050678ca5262d603ecd
Yngrid Padilha <yngrid_padilha@hotmail.com>
2025-05-20 22:24:30 -0300

    Movie, ORM

---
+++ src/main/java/com/movies/movies/entities/Genre.java
@@ -0,0 +1,13 @@
+package com.movies.movies.entities;
+
+public enum Genre {
+
+	ACAO,
+	ROMANCE,
+	TERROR,
+	COMEDIA,
+	ANIMACAO,
+	DOCUMENTARIO,
+	FICCAO_CIENTIFICA
+	
+}
---
+++ src/main/java/com/movies/movies/entities/Movie.java
@@ -0,0 +1,102 @@
+package com.movies.movies.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name="tb_movie")
+public class Movie {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank
+	private String title;
+	
+	private Genre genre;
+	
+	@Column(name="movie_year")
+	private Integer year;
+	
+	private String director;
+	
+	public Movie() {
+		
+	}
+
+	public Movie(Long id, String title, Genre genre, Integer year, String director) {
+		this.id = id;
+		this.title = title;
+		this.genre = genre;
+		this.year = year;
+		this.director = director;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Genre getGenre() {
+		return genre;
+	}
+
+	public void setGenre(Genre genre) {
+		this.genre = genre;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+
+	public String getDirector() {
+		return director;
+	}
+
+	public void setDirector(String director) {
+		this.director = director;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Movie other = (Movie) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+
+}
